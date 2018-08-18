bce6d75c1a0e0ac83efa2cb92d5b1ae3f36cdbbf
Hp <mahshidga@gmail.com>
2018-08-18 15:20:26 +0430

    product detail xml added

---
+++ app/src/main/java/com/yaratech/yaratube/productdetail/ProductDetailFragment.java
@@ -0,0 +1,30 @@
+package com.yaratech.yaratube.productdetail;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yaratech.yaratube.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProductDetailFragment extends Fragment {
+
+
+    public ProductDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product_detail, container, false);
+    }
+
+}
