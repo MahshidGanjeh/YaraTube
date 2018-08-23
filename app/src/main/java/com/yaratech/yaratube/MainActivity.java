2a11b8b37772f36256108c3b3d66622e3cfb7b55
Hp <mahshidga@gmail.com>
2018-08-23 20:05:17 +0430

    login dialog fragment added

--- app/src/main/java/com/yaratech/yaratube/MainActivity.java
+++ app/src/main/java/com/yaratech/yaratube/MainActivity.java
@@ -1,21 +1,30 @@
 package com.yaratech.yaratube;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 
 import com.yaratech.yaratube.gridproduct.GridProductFragment;
 import com.yaratech.yaratube.home.HomeFragment;
+import com.yaratech.yaratube.login.LoginDialogFragment;
 import com.yaratech.yaratube.productdetail.ProductDetailFragment;
 
 public class MainActivity extends AppCompatActivity implements
         onCategoryClickListener, onProductClickListener {
 
-    FragmentManager manager = getSupportFragmentManager();
+
     private GridProductFragment mGridProductFragment;
     private ProductDetailFragment productDetailFragment;
+    private NavigationView mDrawerNavigationView;
+    private DrawerLayout drawer;
+
+    FragmentManager manager = getSupportFragmentManager();
 
     //@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
@@ -27,6 +36,25 @@ public class MainActivity extends AppCompatActivity implements
         // getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
 
         manager.beginTransaction().replace(R.id.main_container, new HomeFragment()).commit();
+
+
+        mDrawerNavigationView = findViewById(R.id.drawer_navigation_view);
+        drawer = findViewById(R.id.drawer);
+
+        mDrawerNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.drawer_profile:
+                        LoginDialogFragment dialogFragment = new LoginDialogFragment();
+                        dialogFragment.show(manager.beginTransaction(),"dialog");
+                        drawer.closeDrawers();
+                        return true;
+                    case R.id.drawer_aboutous:
+                }
+                return false;
+            }
+        });
 
     }
 
--- app/src/main/java/com/yaratech/yaratube/login/LoginDialogFragment.java
+++ app/src/main/java/com/yaratech/yaratube/login/LoginDialogFragment.java
@@ -2,6 +2,8 @@ package com.yaratech.yaratube.login;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,12 +11,18 @@ import android.view.ViewGroup;
 
 import com.yaratech.yaratube.R;
 
-public class LoginDialogFragment extends Fragment {
+public class LoginDialogFragment extends DialogFragment {
 
 
     public LoginDialogFragment() {
         // Required empty public constructor
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
