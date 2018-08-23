5f1e0ec0885f3ab29e6c71bcf3163ecbde2c0b0c
Hp <mahshidga@gmail.com>
2018-08-23 19:36:21 +0430

    login dialog fragment xml addede

---
+++ app/src/main/java/com/yaratech/yaratube/login/LoginDialogFragment.java
@@ -0,0 +1,25 @@
+package com.yaratech.yaratube.login;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yaratech.yaratube.R;
+
+public class LoginDialogFragment extends Fragment {
+
+
+    public LoginDialogFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_dialoug, container, false);
+    }
+
+}
