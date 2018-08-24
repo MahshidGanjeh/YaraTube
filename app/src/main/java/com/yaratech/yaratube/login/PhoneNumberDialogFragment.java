4856fa765d2a72d88b5efc7b9a4c7af8f92fd35b
Hp <mahshidga@gmail.com>
2018-08-24 12:59:25 +0430

    login dialog fragments just added

---
+++ app/src/main/java/com/yaratech/yaratube/login/PhoneNumberDialogFragment.java
@@ -0,0 +1,28 @@
+package com.yaratech.yaratube.login;
+
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yaratech.yaratube.R;
+
+public class PhoneNumberDialogFragment extends DialogFragment {
+
+
+    public PhoneNumberDialogFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_phone_number_login_dialog, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/yaratech/yaratube/login/VerificationCodeDialogFragment.java
@@ -0,0 +1,28 @@
+package com.yaratech.yaratube.login;
+
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yaratech.yaratube.R;
+
+public class VerificationCodeDialogFragment extends DialogFragment {
+
+
+    public VerificationCodeDialogFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_verification_code_dialog, container, false);
+    }
+
+}
